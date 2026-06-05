2a8943768e6e5349d20d4735e14c577dd8124330
Data080826 <dgusa2009@gmail.com>
2026-06-05 11:39:52 -0700

    pubmed.py

--- pubmed.py
+++ pubmed.py
@@ -1,6 +1,6 @@
 from Bio import Entrez
 
-Entrez.email = "your_email@example.com"
+Entrez.email = "dilyarangalimova@gmail.com"
 
 def search_pubmed(query, max_results=20):
     handle = Entrez.esearch(
