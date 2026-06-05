c2e136d1808bdbabfd9b446f451d20192d031c9f
Data080826 <dgusa2009@gmail.com>
2026-06-05 14:54:08 -0700

    app.py

--- app.py
+++ app.py
@@ -309,10 +309,11 @@ if uploaded_file:
         with st.spinner(f"Searching {database}..."):
 
             if database == "PubMed":
-                papers = search_pubmed(
-                    literature_query,
-                    max_results
-                )
+               papers = search_ncbi_database(
+                    query=literature_query,
+                    database=database,
+                    max_results=max_results
+               )
 
             elif database == "PubMed Central (PMC)":
                 papers = search_pmc(
